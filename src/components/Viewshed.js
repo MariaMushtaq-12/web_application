1e34e26edbf0df7398bb2a8c911a05d2230cf9dc
MariaMushtaq-12 <117312073+MariaMushtaq-12@users.noreply.github.com>
2024-07-15 15:11:36 +0500

    Update Viewshed.js

--- src/components/Viewshed.js
+++ src/components/Viewshed.js
@@ -162,14 +162,14 @@ const ViewshedTool = ({  setViewshedParams, clickedCoordinates }) => {
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="w-full bg-gray-600 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
                             Viewshed Analysis
                         </h1>
                         <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                             <div>
-                                <label htmlFor="lat" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Latitude:</label>
+                                <label htmlFor="lat" className="block mb-2 text-sm font-medium text-white dark:text-white">Latitude:</label>
                                 <input
                                     type="text"
                                     id="lat"
@@ -181,7 +181,7 @@ const ViewshedTool = ({  setViewshedParams, clickedCoordinates }) => {
                                 />
                             </div>
                             <div>
-                                <label htmlFor="lon" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Longitude:</label>
+                                <label htmlFor="lon" className="block mb-2 text-sm font-medium text-white dark:text-white">Longitude:</label>
                                 <input
                                     type="text"
                                     id="lon"
@@ -193,7 +193,7 @@ const ViewshedTool = ({  setViewshedParams, clickedCoordinates }) => {
                                 />
                             </div>
                             <div>
-                                <label htmlFor="radius" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Radius (m):</label>
+                                <label htmlFor="radius" className="block mb-2 text-sm font-medium text-white dark:text-white">Radius (m):</label>
                                 <input
                                     type="text"
                                     id="radius"
@@ -205,7 +205,7 @@ const ViewshedTool = ({  setViewshedParams, clickedCoordinates }) => {
                                 />
                             </div>
                             <div>
-                                <label htmlFor="height" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Height Above Ground (m):</label>
+                                <label htmlFor="height" className="block mb-2 text-sm font-medium text-white dark:text-white">Height Above Ground (m):</label>
                                 <input
                                     type="text"
                                     id="height"
@@ -216,7 +216,7 @@ const ViewshedTool = ({  setViewshedParams, clickedCoordinates }) => {
                                     required
                                 />
                             </div>
-                            <button type="submit" className="w-full text-white bg-black  hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                            <button type="submit" className="w-full text-white bg-black  hover:bg-green-500 hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                                 Run Viewshed
                             </button>
                             </form>
