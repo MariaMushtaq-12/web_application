14356ca677f4d678e1dd4d4d89f6dba153b57710
MariaMushtaq-12 <117312073+MariaMushtaq-12@users.noreply.github.com>
2024-07-15 18:17:36 +0500

    form only

--- src/components/Eptool.js
+++ src/components/Eptool.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 
-const EpTool = ({ setEpToolParams, clickedCoordinates }) => {
+const EpTool = ({ setEpToolParams, clickedCoordinates,onClose }) => {
   const [startLat, setStartLat] = useState('');
   const [startLon, setStartLon] = useState('');
   const [endLat, setEndLat] = useState('');
@@ -32,25 +32,72 @@ const EpTool = ({ setEpToolParams, clickedCoordinates }) => {
 
   return (
     <div className="ep-tool">
-      <form onSubmit={handleSubmit}>
-        <label>
-          Start Latitude:
-          <input type="number" value={startLat} onChange={(e) => setStartLat(e.target.value)} />
-        </label>
-        <label>
-          Start Longitude:
-          <input type="number" value={startLon} onChange={(e) => setStartLon(e.target.value)} />
-        </label>
-        <label>
-          End Latitude:
-          <input type="number" value={endLat} onChange={(e) => setEndLat(e.target.value)} />
-        </label>
-        <label>
-          End Longitude:
-          <input type="number" value={endLon} onChange={(e) => setEndLon(e.target.value)} />
-        </label>
-        <button type="submit">Calculate Line of Sight</button>
+      <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="w-full bg-gray-600 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+       <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
+              Elevation Profile
+            </h1>
+     <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}> 
+      <div className="grid grid-cols-2 gap-4">
+      <div>
+                  <label htmlFor="startLat" className="block mb-2 text-sm font-medium text-white dark:text-white">Start Latitude:</label>
+                  <input
+                    type="number"
+                    id="startLat"
+                    value={startLat}
+                    onChange={(e) => setStartLat(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter start latitude"
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="startLon" className="block mb-2 text-sm font-medium text-white dark:text-white">Start Longitude:</label>
+                  <input
+                    type="number"
+                    id="startLon"
+                    value={startLon}
+                    onChange={(e) => setStartLon(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter start longitude"
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="endLat" className="block mb-2 text-sm font-medium text-white dark:text-white">End Latitude:</label>
+                  <input
+                    type="number"
+                    id="endLat"
+                    value={endLat}
+                    onChange={(e) => setEndLat(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter end latitude"
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="endLon" className="block mb-2 text-sm font-medium text-white dark:text-white">End Longitude:</label>
+                  <input
+                    type="number"
+                    id="endLon"
+                    value={endLon}
+                    onChange={(e) => setEndLon(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter end longitude"
+                    required
+                  />
+                </div>
+</div>
+
+        <button type="submit" className="w-full text-white bg-black  hover:bg-green-500 hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Calculate Line of Sight</button>
+        <button onClick={onClose} className="w-full text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-4">
+            Close
+          </button>
       </form>
+</div>
+</div>
+</section>
     </div>
   );
 };
