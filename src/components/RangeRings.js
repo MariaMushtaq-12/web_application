20de358aa7d0eb4a2441171fecb59bb2d56541be
MariaMushtaq-12 <117312073+MariaMushtaq-12@users.noreply.github.com>
2024-07-15 15:11:33 +0500

    Update RangeRings.js

--- src/components/RangeRings.js
+++ src/components/RangeRings.js
@@ -108,14 +108,14 @@ const RangeRingsTool = ({ setRangeRingsParams, clickedCoordinates }) => {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-gray-600 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
               Create Range Rings
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
-                <label htmlFor="latitude" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Latitude:</label>
+                <label htmlFor="latitude" className="block mb-2 text-sm font-medium text-white dark:text-white">Latitude:</label>
                 <input
                   type="number"
                   step="any"
@@ -128,7 +128,7 @@ const RangeRingsTool = ({ setRangeRingsParams, clickedCoordinates }) => {
                 />
               </div>
               <div>
-                <label htmlFor="longitude" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Longitude:</label>
+                <label htmlFor="longitude" className="block mb-2 text-sm font-medium text-white dark:text-white">Longitude:</label>
                 <input
                   type="number"
                   step="any"
@@ -141,7 +141,7 @@ const RangeRingsTool = ({ setRangeRingsParams, clickedCoordinates }) => {
                 />
               </div>
               <div>
-                <label htmlFor="radius" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Radius (meters):</label>
+                <label htmlFor="radius" className="block mb-2 text-sm font-medium text-white dark:text-white">Radius (meters):</label>
                 <input
                   type="number"
                   step="any"
@@ -154,7 +154,7 @@ const RangeRingsTool = ({ setRangeRingsParams, clickedCoordinates }) => {
                 />
               </div>
               <div>
-                <label htmlFor="rings" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of Rings:</label>
+                <label htmlFor="rings" className="block mb-2 text-sm font-medium text-white dark:text-white">Number of Rings:</label>
                 <input
                   type="number"
                   step="1"
@@ -166,7 +166,7 @@ const RangeRingsTool = ({ setRangeRingsParams, clickedCoordinates }) => {
                   required
                 />
               </div>
-              <button type="submit" className="w-full text-white bg-black  hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+              <button type="submit" className="w-full text-white bg-black hover:bg-green-500 hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                 Create Range Rings
               </button>
             </form>
